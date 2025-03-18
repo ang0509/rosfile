8f5cee7fc91b8d94d196407f30be3e4655b44964
Hilcorpter <1830194362@qq.com>
2025-03-18 16:23:14 +0800

    code fix

--- src/getter/getter/getter.py
+++ src/getter/getter/getter.py
@@ -15,3 +15,25 @@ def main(args=None):
         node=getterNode("getter")
         rclpy.spin(node) 
         rclpy.shutdown() 
+
+int main() {
+    int num1, num2;
+    
+    
+    std::cout << "请输入两个整数：" << std::endl;
+    std::cin >> num1 >> num2;
+    
+    // 计算并输出结果
+    std::cout << "数字 " << num1 << " 和 " << num2 << " 的和为：" << num1 + num2 << std::endl;
+    std::cout << "数字 " << num1 << " 和 " << num2 << " 的差为：" << num1 - num2 << std::endl;
+    std::cout << "数字 " << num1 << " 和 " << num2 << " 的乘积为：" << num1 * num2 << std::endl;
+    
+    // 判断除数是否为0，避免除以0的错误
+    if (num2 != 0) {
+        std::cout << "数字 " << num1 << " 除以 " << num2 << " 的商为：" << num1 / num2 << std::endl;
+    } else {
+        std::cout << "除数不能为0！" << std::endl;
+    }
+    
+    return 0;
+}
